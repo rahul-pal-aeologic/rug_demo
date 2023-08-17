0c49f61767e02a8e5bd1eba73ffdb826c6d82033
Devesh Kumar Patel <devesh@aeologic.com>
2023-08-17 18:09:12 +0530

    page added

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,13 +1,17 @@
 import 'package:go_router/go_router.dart';
 import 'package:rug_demo/main.dart';
+import 'package:rug_demo/presentation/login/view/login_page.dart';
+import 'package:rug_demo/presentation/onboarding/view/onboarding_page.dart';
+import 'package:rug_demo/presentation/signup/view/signup_page.dart';
 import 'package:rug_demo/presentation/splash/splash_screen.dart';
 
 final GoRouter router = GoRouter(
-  initialLocation: SplashScreen.routeName,
-  navigatorKey: navigationKey,
-  routes: [
-    GoRoute(
-      name: SplashScreen.routeName,
-      path: SplashScreen.routeName,
-      builder: (context, state) => const SplashScreen(),
-    )]);
+    initialLocation: SplashScreen.routeName,
+    navigatorKey: navigationKey,
+    routes: [
+      GoRoute(
+        name: SplashScreen.routeName,
+        path: SplashScreen.routeName,
+        builder: (context, state) => const OnboardingScreen(),
+      )
+    ]);
