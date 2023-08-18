7fc0d99a6273386f0da62a11e673dcace6767959
Rahul Pal <rahul.pal@aeologic.com>
2023-08-18 11:53:38 +0530

    filter routing added

--- lib/presentation/filter/view/filter_page.dart
+++ lib/presentation/filter/view/filter_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:rug_demo/global/resource/app_colors.dart';
 import 'package:rug_demo/presentation/common_widgets/primary_button.dart';
 
@@ -14,183 +15,187 @@ class _FilterScreenState extends State<FilterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 35),
-            child: Row(
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.arrow_back_ios_new,
-                  ),
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                const Text(
-                  "Filters",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: AppColors.blackColor,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w800),
-                ),
-              ],
-            ),
-          ),
-          Divider(
-            thickness: 1,
-            color: Colors.black.withOpacity(.5),
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 35),
+              child: Row(
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Row(
-                      children: [
-                        Text(
-                          "DESIGINERS",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                              color: AppColors.blackColor,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w800),
-                        ),
-                        SizedBox(
-                          width: 42,
-                        ),
-                        Text(
-                          "3",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(
-                              color: AppColors.blackColor,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w800),
-                        ),
-                      ],
+                  IconButton(
+                    onPressed: () {
+                      context.pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new,
                     ),
                   ),
-                  Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            color: AppColors.greyShade1.withOpacity(.3)),
-                        width: 170,
-                        height: 742,
-                        child: const Column(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(
-                                  left: 15, right: 12, top: 18, bottom: 20),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "CATEGORIES",
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                        color: AppColors.greyColor,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w800),
-                                  ),
-                                  Text(
-                                    "4",
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                        color: AppColors.greyColor,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w800),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  EdgeInsets.only(left: 15, right: 12, top: 8),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "COLOR",
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                        color: AppColors.greyColor,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w800),
-                                  ),
-                                  Text(
-                                    "2",
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                        color: AppColors.greyColor,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w800),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  const Text(
+                    "Filters",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: AppColors.blackColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800),
                   ),
                 ],
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _listItem(
-                    "assets/greycircle.png",
-                    "Allon Gullery",
-                  ),
-                  _listItem(
-                    "assets/greycircle.png",
-                    "Ayeno Murayama",
-                  ),
-                  _listItem(
-                    "assets/bluecircle.png",
-                    "Fractal Kust",
-                  ),
-                  _listItem(
-                    "assets/bluecircle.png",
-                    "Ibrahim Hassam",
-                  ),
-                  _listItem(
-                    "assets/bluecircle.png",
-                    "Locand Nagy",
-                  ),
-                  _listItem(
-                    "assets/greycircle.png",
-                    "Make Gardon",
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 420, left: 30),
-                    child: SizedBox(
-                      width: 180,
-                      height: 60,
-                      child: PrimaryButton(
-                        label: "",
-                        label2: "Apply Filter",
-                        onPressed: () {
-                          Navigator.of(context);
-                        },
-                        color: AppColors.primaryColor,
+            ),
+            Divider(
+              thickness: 1,
+              color: Colors.black.withOpacity(.5),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "DESIGINERS",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                color: AppColors.blackColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w800),
+                          ),
+                          SizedBox(
+                            width: 42,
+                          ),
+                          Text(
+                            "3",
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                                color: AppColors.blackColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w800),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
-          )
-        ],
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: AppColors.greyShade1.withOpacity(.3)),
+                          width: 170,
+                          height: 742,
+                          child: const Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    left: 15, right: 12, top: 18, bottom: 20),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "CATEGORIES",
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                          color: AppColors.greyColor,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                    Text(
+                                      "4",
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                          color: AppColors.greyColor,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    EdgeInsets.only(left: 15, right: 12, top: 8),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "COLOR",
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                          color: AppColors.greyColor,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                    Text(
+                                      "2",
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                          color: AppColors.greyColor,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _listItem(
+                      "assets/greycircle.png",
+                      "Allon Gullery",
+                    ),
+                    _listItem(
+                      "assets/greycircle.png",
+                      "Ayeno Murayama",
+                    ),
+                    _listItem(
+                      "assets/bluecircle.png",
+                      "Fractal Kust",
+                    ),
+                    _listItem(
+                      "assets/bluecircle.png",
+                      "Ibrahim Hassam",
+                    ),
+                    _listItem(
+                      "assets/bluecircle.png",
+                      "Locand Nagy",
+                    ),
+                    _listItem(
+                      "assets/greycircle.png",
+                      "Make Gardon",
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 420, left: 30),
+                      child: SizedBox(
+                        width: 180,
+                        height: 60,
+                        child: PrimaryButton(
+                          label: "",
+                          label2: "Apply Filter",
+                          onPressed: () {
+                            Navigator.of(context);
+                          },
+                          color: AppColors.primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
