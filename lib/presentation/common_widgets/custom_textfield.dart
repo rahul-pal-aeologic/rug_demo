393566c32a97a59efacbdb605600ef02272477a1
Devesh Kumar Patel <devesh@aeologic.com>
2023-08-18 12:53:55 +0530

    code

--- lib/presentation/common_widgets/custom_textfield.dart
+++ lib/presentation/common_widgets/custom_textfield.dart
@@ -38,6 +38,8 @@ class CustomTextField extends StatelessWidget {
   final Widget? prefixIcon;
   final double? height;
   final int? maxLength;
+  final Color? boxColor;
+
   List<TextInputFormatter>? inputFormatters;
 
   CustomTextField({
@@ -71,6 +73,7 @@ class CustomTextField extends StatelessWidget {
     this.enabled = true,
     this.hintColor,
     this.height,
+    this.boxColor = AppColors.mainBackgroundColor,
   });
 
   @override
@@ -105,11 +108,12 @@ class CustomTextField extends StatelessWidget {
               )
             : const SizedBox.shrink(),
         Container(
+          height: height,
           width: MediaQuery.of(context).size.width * 0.9,
           decoration: BoxDecoration(
-            color: AppColors.mainBackgroundColor,
+            color: boxColor,
             borderRadius: BorderRadius.circular(14),
-            border: Border.all(color: Colors.grey, width: 0),
+            border: Border.all(color: Colors.grey, width: 2),
           ),
           child: Padding(
             padding: const EdgeInsets.all(8),
@@ -153,6 +157,7 @@ class CustomTextField extends StatelessWidget {
                             textOverflow: TextOverflow.ellipsis),
                         decoration: InputDecoration(
                           hintText: hintText,
+
                           // suffix: Padding(
                           //   padding: EdgeInsets.only(left: 10, right: 10),
                           //   child: suffixIcon ??
